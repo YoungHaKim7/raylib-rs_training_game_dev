63603f0507ccfe139f698d64e488dc2a87e01953
GYoung <ytok1108@aol.com>
2025-01-05 14:26:02 +0900

    Color랑 분리 중

--- 04_Cpp_raylib/02_cpp_raylib_LinuxOS/001_Tutorials_raylib_cpp/a01_tetris/src/grid.cpp
+++ 04_Cpp_raylib/02_cpp_raylib_LinuxOS/001_Tutorials_raylib_cpp/a01_tetris/src/grid.cpp
@@ -1,6 +1,8 @@
-#include "headers/grid.h"
 #include <iostream>
 #include <raylib.h>
+
+#include "headers/grid.h"
+#include "headers/color.h"
 
 Grid::Grid()
 {
@@ -28,20 +30,6 @@ void Grid::Print()
         }
         std::cout << std::endl;
     }
-}
-
-std::vector<Color> Grid::GetCellColors()
-{
-    Color darkGrey = { 26, 31, 40, 255 };
-    Color green = { 47, 230, 23, 255 };
-    Color red = { 232, 18, 18, 255 };
-    Color orange = { 226, 116, 17, 255 };
-    Color yellow = { 237, 234, 4, 255 };
-    Color purple = { 166, 0, 247, 255 };
-    Color cyan = { 21, 204, 209, 255 };
-    Color blue = { 13, 230, 23, 255 };
-
-    return { darkGrey, green, red, orange, yellow, purple, cyan, blue };
 }
 
 void Grid::Draw()
---
+++ 04_Cpp_raylib/02_cpp_raylib_LinuxOS/001_Tutorials_raylib_cpp/a01_tetris/src/position.cpp
@@ -0,0 +1,7 @@
+#include "headers/position.h"
+
+Position::Position(int row, int column)
+{
+    this -> row = row;
+    this -> column = column;
+}
