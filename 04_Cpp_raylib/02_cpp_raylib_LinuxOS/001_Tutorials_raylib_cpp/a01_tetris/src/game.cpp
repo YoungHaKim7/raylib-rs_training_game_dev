c3b41f4e59a8a10cbb94df8f3a5ab6219d5ab2e9
GYoung <ytok1108@aol.com>
2025-01-05 22:55:16 +0900

    블록 쌓이고 다른 랜덤 블럭 잘 내려옴

--- 04_Cpp_raylib/02_cpp_raylib_LinuxOS/001_Tutorials_raylib_cpp/a01_tetris/src/game.cpp
+++ 04_Cpp_raylib/02_cpp_raylib_LinuxOS/001_Tutorials_raylib_cpp/a01_tetris/src/game.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 
 #include "headers/game.h"
+#include "headers/position.h"
 
 Game::Game()
 {
@@ -37,48 +38,44 @@ void Game::Draw()
 void Game::HandleInput()
 {
     int keyPressed = GetKeyPressed();
-    switch(keyPressed)
-    {
+    switch (keyPressed) {
 
-        case KEY_LEFT:
-            MoveBlockLeft();
-            break;
-        case KEY_RIGHT:
-            MoveBlockRight();
-            break;
-        case KEY_DOWN:
-            MoveBlockDown();
-            break;
-        case KEY_UP:
-            RotateBlock();
-            break;
+    case KEY_LEFT:
+        MoveBlockLeft();
+        break;
+    case KEY_RIGHT:
+        MoveBlockRight();
+        break;
+    case KEY_DOWN:
+        MoveBlockDown();
+        break;
+    case KEY_UP:
+        RotateBlock();
+        break;
     }
 }
 
 void Game::MoveBlockLeft()
 {
     currentBlock.Move(0, -1);
-    if(IsBlockOutside())
-    {
-        currentBlock.Move(0,1);
+    if (IsBlockOutside()) {
+        currentBlock.Move(0, 1);
     }
 }
 
 void Game::MoveBlockRight()
 {
     currentBlock.Move(0, 1);
-    if(IsBlockOutside())
-    {
-        currentBlock.Move(0,-1);
+    if (IsBlockOutside()) {
+        currentBlock.Move(0, -1);
     }
 }
 
 void Game::MoveBlockDown()
 {
     currentBlock.Move(1, 0);
-    if(IsBlockOutside())
-    {
-        currentBlock.Move(-1,0);
+    if (IsBlockOutside() || BlockFits() == false) {
+        currentBlock.Move(-1, 0);
         LockBlock();
     }
 }
@@ -86,19 +83,16 @@ void Game::MoveBlockDown()
 void Game::MoveBlockUp()
 {
     currentBlock.Move(-1, 0);
-    if(IsBlockOutside())
-    {
-        currentBlock.Move(1,0);
+    if (IsBlockOutside()) {
+        currentBlock.Move(1, 0);
     }
 }
 
 bool Game::IsBlockOutside()
 {
     std::vector<Position> tiles = currentBlock.GetCellPositions();
-    for(Position item: tiles)
-    {
-        if(grid.IsCellOutside(item.row, item.column))
-        {
+    for (Position item : tiles) {
+        if (grid.IsCellOutside(item.row, item.column)) {
             return true;
         }
     }
@@ -108,8 +102,7 @@ bool Game::IsBlockOutside()
 void Game::RotateBlock()
 {
     currentBlock.Rotate();
-    if(IsBlockOutside())
-    {
+    if (IsBlockOutside()) {
         currentBlock.UndoRotation();
     }
 }
@@ -117,10 +110,20 @@ void Game::RotateBlock()
 void Game::LockBlock()
 {
     std::vector<Position> tiles = currentBlock.GetCellPositions();
-    for(Position item: tiles)
-    {
+    for (Position item : tiles) {
         grid.grid[item.row][item.column] = currentBlock.id;
     }
     currentBlock = nextBlock;
     nextBlock = GetRandomBlock();
+}
+
+bool Game::BlockFits()
+{
+    std::vector<Position> tiles = currentBlock.GetCellPositions();
+    for (Position item : tiles) {
+        if (grid.IsCellEmpty(item.row, item.column) == false) {
+            return false;
+        }
+    }
+    return true;
 }
